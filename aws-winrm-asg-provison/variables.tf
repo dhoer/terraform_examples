d899b8083177c8ca80371f2d3448d6f2254faf8a
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-22 13:05:11 -0700

    Add aws-winrm-asg-provision outline

---
+++ aws-winrm-asg-provison/variables.tf
@@ -0,0 +1,19 @@
+variable "admin_password" {
+  description = "Windows Administrator password to login as."
+}
+
+variable "key_name" {
+  description = "Name of the SSH keypair to use in AWS."
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default = "us-east-1"
+}
+
+# Windows Server 2012 R2 Base
+variable "aws_amis" {
+  default = {
+    us-east-1 = "ami-5b9e6b30"
+  }
+}
