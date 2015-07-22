55474ec0c7593d36804346b0f5e193398f77ea59
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-22 12:26:59 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Terraform
+
+The missing WinRm Terraform examples.  Feel free create an issue or pull request if want to add an example or have a 
+better way to accomplish something. The more feedback, the better for me and everyone else. ;-)
+
+## Examples
+
+- instance.tf - Shows how to use `user_data` to configure WinRM and firewall for AWS Window 2012R2 Base image.
---
+++ instance.tf
@@ -0,0 +1,64 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name = "terraform_example"
+  description = "Used in the terraform"
+
+  # WinRM access from anywhere
+  ingress {
+    from_port = 5985
+    to_port = 5985
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    type = "winrm"
+    user = "Administrator"
+    password = "${var.admin_password}"
+  }
+
+  instance_type = "m1.small"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+
+  # The name of our SSH keypair you've created and downloaded
+  # from the AWS console.
+  #
+  # https://console.aws.amazon.com/ec2/v2/home?region=us-west-2#KeyPairs:
+  #
+  key_name = "${var.key_name}"
+
+  # Our Security group to allow WinRM access
+  security_groups = ["${aws_security_group.default.name}"]
+
+  user_data = <<-EOF
+    <script>
+      winrm quickconfig -q & winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"} & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"}
+    </script>
+    <powershell>
+      netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow
+      $admin = [adsi]("WinNT://./administrator, user")
+      $admin.psbase.invoke("SetPassword", "${var.admin_password}")
+    </powershell>
+  EOF
+}
